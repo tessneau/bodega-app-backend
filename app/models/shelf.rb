dcdfb91f9cbc01c6186d928b2764663fb23a0517
Tess Neau <tessneau@gmail.com>
2019-07-30 18:02:41 -0400

    added Shelves model

---
+++ app/models/shelf.rb
@@ -0,0 +1,3 @@
+class Shelf < ApplicationRecord
+  has_many :categories
+end
--- db/migrate/20190726205618_create_categories.rb
+++ db/migrate/20190726205618_create_categories.rb
@@ -2,6 +2,7 @@ class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
       t.string :name
+      t.references :shelf, foreign_key: true
 
       t.timestamps
     end
