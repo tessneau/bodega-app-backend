77dabebe5ea6e0808a08d4be0a0310e936fdb283
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-07-31 19:19:08 -0400

    pulled 1 more time

--- db/migrate/20190726210047_create_cart_items.rb
+++ db/migrate/20190726210047_create_cart_items.rb
@@ -2,6 +2,7 @@ class CreateCartItems < ActiveRecord::Migration[5.2]
   def change
     create_table :cart_items do |t|
       t.string :name
+      t.string :img_url
       t.decimal :price, precision: 10, scale: 2, default: 0.00
       t.references :cart, foreign_key: true
       t.references :item, foreign_key: true
